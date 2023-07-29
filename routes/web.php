86b1a0dce0d5e6c69563896933ff960c65845ef1
rohitvishin <rohitvishwakarma65@gmail.com>
2023-07-30 00:13:16 +0530

    user auth middleware

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,39 @@ use App\Http\Controllers\FrontendController;
 */
 
 // Basic Website Routes
-Route::get('/', [FrontendController::class,'index']);
-Route::get('/login', [FrontendController::class,'login']);
-Route::get('/register', [FrontendController::class,'register']);
-Route::get('/shop', [FrontendController::class,'shop']);
-Route::get('/categories', [FrontendController::class,'categories']);
-Route::get('/product-details/{product_id}', [FrontendController::class,'index']);
-Route::get('/category-products/{category_id}', [FrontendController::class,'index']);
-Route::get('/bulk-order', [FrontendController::class,'index']);
-Route::get('/blog', [FrontendController::class,'index']);
-Route::get('/blog-detail/{blog_id}', [FrontendController::class,'index']);
-Route::get('/faq', [FrontendController::class,'index']);
+Route::get('/', [FrontendController::class, 'index']);
+Route::get('/login', [FrontendController::class, 'login']);
+Route::get('/register', [FrontendController::class, 'register']);
+Route::get('/shop', [FrontendController::class, 'shop']);
+Route::get('/categories', [FrontendController::class, 'categories']);
+Route::get('/product-details/{product_id}', [FrontendController::class, 'index']);
+Route::get('/category-products/{category_id}', [FrontendController::class, 'index']);
+Route::get('/bulk-order', [FrontendController::class, 'index']);
+Route::get('/blog', [FrontendController::class, 'index']);
+Route::get('/blog-detail/{blog_id}', [FrontendController::class, 'index']);
+Route::get('/faq', [FrontendController::class, 'index']);
 
 // User Routes
-Route::get('/my-profile', [FrontendController::class,'profile']);
-Route::get('/my-cart', [FrontendController::class,'index']);
-Route::get('/my-wishlist', [FrontendController::class,'index']);
-Route::get('/my-orders', [FrontendController::class,'index']);
-Route::get('/checkout', [FrontendController::class,'index']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/my-profile', [FrontendController::class, 'profile']);
+    Route::get('/my-cart', [FrontendController::class, 'index']);
+    Route::get('/my-wishlist', [FrontendController::class, 'index']);
+    Route::get('/my-orders', [FrontendController::class, 'index']);
+    Route::get('/checkout', [FrontendController::class, 'index']);
+});
 
 // Comapany Static Pages Routes
-Route::get('/terms-and-conditions', [FrontendController::class,'index']);
-Route::get('/refund-policy', [FrontendController::class,'index']);
-Route::get('/shipping-policy', [FrontendController::class,'index']);
-Route::get('/privacy-policy', [FrontendController::class,'index']);
-Route::get('/about-us', [FrontendController::class,'index']);
-Route::get('/contact-us', [FrontendController::class,'index']);
+Route::get('/terms-and-conditions', [FrontendController::class, 'index']);
+Route::get('/refund-policy', [FrontendController::class, 'index']);
+Route::get('/shipping-policy', [FrontendController::class, 'index']);
+Route::get('/privacy-policy', [FrontendController::class, 'index']);
+Route::get('/about-us', [FrontendController::class, 'index']);
+Route::get('/contact-us', [FrontendController::class, 'index']);
 
 
 // Accounts Routes
-Route::group(['prefix' => 'accounts'], function(){  
-    Route::get('/contact-us', [FrontendController::class,'index']);
-});  
+Route::group(['prefix' => 'accounts'], function () {
+    Route::get('/contact-us', [FrontendController::class, 'index']);
+});
+
+
